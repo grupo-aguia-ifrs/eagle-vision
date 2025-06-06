29e439627390ac18a7588420105261609b57a73e
PabloGomesz <gomesdossantospablo77@gmail.com>
2025-06-06 11:34:24 -0300

    feat: Ajuste na model cftv

--- backend/alarme/models.py
+++ backend/alarme/models.py
@@ -4,7 +4,7 @@ import uuid
 
 class AlarmeModel(models.Model):
     id  = models.AutoField(primary_key=True)
-    id_cliente = models.ForeignKey(ClienteModel, on_delete=models.CASCADE)
+    id_cliente = models.ForeignKey('cliente.ClienteModel', on_delete=models.CASCADE)
     modelo_central = models.CharField(max_length=255)
     sensores_instalados = models.CharField(max_length=255)
     grup_pessoas = models.CharField(max_length=255)
